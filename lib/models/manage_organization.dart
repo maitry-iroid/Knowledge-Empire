2e7143ada718482010aa71078ea983e6478f3b7c
Riddhi Shankar <riddhi@iroidsolutions.com>
2020-01-01 14:45:30 +0530

    @riddhi : manage module permission

--- lib/models/manage_organization.dart
+++ lib/models/manage_organization.dart
@@ -22,7 +22,6 @@ class ManageOrganizationRequest {
   }
 }
 
-
 class ManageOrgResponse {
   String flag;
   String result;
@@ -35,7 +34,8 @@ class ManageOrgResponse {
     flag = json['flag'];
     result = json['result'];
     msg = json['msg'];
-    data = json['data'] != null ? new ManageOrgData.fromJson(json['data']) : null;
+    data =
+        json['data'] != null ? new ManageOrgData.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -53,27 +53,30 @@ class ManageOrgResponse {
 class ManageOrgData {
   int totalEmployeeCapacity;
   int remainingEmployeeCapacity;
-  int totalCustomerCapicity;
-  int remainingCustomerCapicity;
-  int remianingSalesPerson;
+  int totalCustomerCapacity;
+  int remainingCustomerCapacity;
+  int totalSalesPerson;
+  int remainingSalesPerson;
   int totalBalance;
   List<Organization> organization;
 
   ManageOrgData(
       {this.totalEmployeeCapacity,
-        this.remainingEmployeeCapacity,
-        this.totalCustomerCapicity,
-        this.remainingCustomerCapicity,
-        this.remianingSalesPerson,
-        this.totalBalance,
-        this.organization});
+      this.remainingEmployeeCapacity,
+      this.totalCustomerCapacity,
+      this.remainingCustomerCapacity,
+      this.totalSalesPerson,
+      this.remainingSalesPerson,
+      this.totalBalance,
+      this.organization});
 
   ManageOrgData.fromJson(Map<String, dynamic> json) {
     totalEmployeeCapacity = json['totalEmployeeCapacity'];
     remainingEmployeeCapacity = json['remainingEmployeeCapacity'];
-    totalCustomerCapicity = json['totalCustomerCapicity'];
-    remainingCustomerCapicity = json['remainingCustomerCapicity'];
-    remianingSalesPerson = json['remianingSalesPerson'];
+    totalCustomerCapacity = json['totalCustomerCapacity'];
+    remainingCustomerCapacity = json['remainingCustomerCapacity'];
+    totalSalesPerson = json['totalSalesPerson'];
+    remainingSalesPerson = json['remainingSalesPerson'];
     totalBalance = json['totalBalance'];
     if (json['organization'] != null) {
       organization = new List<Organization>();
@@ -87,9 +90,10 @@ class ManageOrgData {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['totalEmployeeCapacity'] = this.totalEmployeeCapacity;
     data['remainingEmployeeCapacity'] = this.remainingEmployeeCapacity;
-    data['totalCustomerCapicity'] = this.totalCustomerCapicity;
-    data['remainingCustomerCapicity'] = this.remainingCustomerCapicity;
-    data['remianingSalesPerson'] = this.remianingSalesPerson;
+    data['totalCustomerCapacity'] = this.totalCustomerCapacity;
+    data['remainingCustomerCapacity'] = this.remainingCustomerCapacity;
+    data['totalSalesPerson'] = this.totalSalesPerson;
+    data['remainingSalesPerson'] = this.remainingSalesPerson;
     data['totalBalance'] = this.totalBalance;
     if (this.organization != null) {
       data['organization'] = this.organization.map((v) => v.toJson()).toList();
@@ -97,4 +101,3 @@ class ManageOrgData {
     return data;
   }
 }
-
