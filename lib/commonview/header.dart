ee1529eb9da44eb01f55a23809d8a063c364d957
Vishal <vishal@iroidsolutions.com>
2020-01-25 15:04:11 +0530

    @vishal : intro screen in changes..

--- lib/commonview/header.dart
+++ lib/commonview/header.dart
@@ -6,7 +6,9 @@ import 'package:ke_employee/helper/constant.dart';
 import 'package:ke_employee/helper/header_utils.dart';
 import 'package:ke_employee/helper/res.dart';
 import 'package:ke_employee/injection/dependency_injection.dart';
+import 'package:ke_employee/screens/home.dart';
 import 'package:ke_employee/screens/intro_screen.dart';
+import 'package:ke_employee/screens/profile.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class HeaderView extends StatefulWidget {
@@ -214,6 +216,8 @@ class HeaderViewState extends State<HeaderView> {
         onTap: () {
           Utils.playClickSound();
           widget.openProfile();
+//          Navigator.push(context, MaterialPageRoute(builder: (context) => FadeRouteHome()));
+
         });
   }
 
