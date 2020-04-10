d084df9c6f003be7751a35cc8cabd39a1895cf70
ravi vithani <ravi.vithani.iroid@gmail.com>
2020-04-10 19:51:14 +0530

    @ravi: Solving issue and making force update dialog.

--- lib/screens/dashboard_prof.dart
+++ lib/screens/dashboard_prof.dart
@@ -146,6 +146,7 @@ class DashboardProfPageState extends State<DashboardProfPage> {
                 overflow: TextOverflow.ellipsis,
               ),
             )
+
           ],
         ),
       ),
