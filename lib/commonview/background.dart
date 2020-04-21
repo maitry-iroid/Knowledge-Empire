dd0148b34c3fe8b3ef0cb8cbd1e7f33c7826e529
ravi vithani <ravi.vithani.iroid@gmail.com>
2020-04-21 16:43:21 +0530

    @ravi: Making issue list

--- lib/commonview/background.dart
+++ lib/commonview/background.dart
@@ -543,6 +543,7 @@ class CommonView {
                                       StringRes.noOffline, context, true);
                                 }
                               });
+
                             },
                           )),
                     ),
