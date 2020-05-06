0d85b1b6205de013117c70b278cd2a315823eaa1
Riddhi Shankar <riddhi@iroidsolutions.com>
2020-05-06 16:23:16 +0530

    @riddhi: version changed

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1302,14 +1302,29 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
+  /*
+  *           IMPORTANT
+  * We introduced the "App version", so that we have a reference we can use to have a common understanding which version we are talking about.
+  * The versioning could work like this:
+  * "A-BBB-C Version: XXX.YYY-ZZZ"
+  * A is replaced with P for Productive, T for Testing (which we do not have at the moment) and D for Development.
+  * BBB is replaced if we have customer specific versions. Standard is BES (for Blue Elephants Solutions)
+  * C is replaced with the target operating system (I for iOS and A for Android). is this enough or should we have more coding for the operating system
+  * (e.g. in case of mayor changes of operating system and we need to have different versions ready for different operating systems?
+  * X= is version numbering for mayor releases. Currently 000. Once we have a version to really go live with it will be 001.
+  * Y= For bigger updated (e.g. new features implemented). Falls back to 000 when X goes up by one
+  * Z= for smaller updates and bug fixes. Falls back to 000 when Y goes up by one.
+  * */
+
+
   String getVersion() {
     String mode = Injector.isDev ? "D" : "P";
     String customerSpecificVersion = "BES"; //Blue Elephants Solutions
     String os = Injector.deviceType == "ios" ? "I" : "A";
 
-    String x = "000";
-    String y = "000";
-    String z = "013";
+    String x = "002";
+    String y = "001";
+    String z = "000";
 
     return mode +
         "-" +
