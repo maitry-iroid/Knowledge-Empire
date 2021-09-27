6784f79a4a751006d8387ef0b81ed58398242861
Riddhi <riddhi@iroidsolutions.com>
2021-09-27 13:17:58 +0530

    @riddhi : fastlane added

---
+++ android/app/src/main/kotlin/com/example/ke_dev2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ke_dev2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+
+// import 'package:background_fetch/background_fetch.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int _status = 0;
+
+  // var firebaseMessaging = Injector.firebaseMessaging;
+
+  @override
+  void initState() {
+    // print("weburl-------" + Const.webUrl);
+
+    //TODO
+    // Injector.audioCache.loadAll(['all_button_clicks.wav']);
+
+    initPlatformState();
+
+    // Injector.getContext(context);
+
+    super.initState();
+    // EnvUtils.getOrientation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<void> initPlatformState() async {
+//     // Configure BackgroundFetch.
+//     BackgroundFetch.configure(
+//             BackgroundFetchConfig(
+//               minimumFetchInterval: 15,
+//               forceAlarmManager: false,
+//               stopOnTerminate: false,
+//               startOnBoot: true,
+//               enableHeadless: true,
+//               requiresBatteryNotLow: false,
+//               requiresCharging: false,
+//               requiresStorageNotLow: false,
+//               requiresDeviceIdle: false,
+//               requiredNetworkType: NetworkType.NONE,
+//             ),
+//             _onBackgroundFetch)
+//         .then((int status) {
+//       print('[BackgroundFetch] configure success: $status');
+//       setState(() {
+//         _status = status;
+//       });
+//     }).catchError((e) {
+//       print('[BackgroundFetch] configure ERROR: $e');
+//       setState(() {
+//         _status = e;
+//       });
+//     });
+//
+// //    BackgroundFetch.scheduleTask(TaskConfig(
+// //        taskId: "com.transistorsoft.customtask",
+// //        delay: 10000,
+// //        periodic: false,
+// //        forceAlarmManager: true,
+// //        stopOnTerminate: false,
+// //        enableHeadless: true));
+//
+//     // Optionally query the current BackgroundFetch status.
+//     int status = await BackgroundFetch.status;
+//     setState(() {
+//       _status = status;
+//     });
+//
+//     // If the widget was removed from the tree while the asynchronous platform
+//     // message was in flight, we want to discard the reply rather than calling
+//     // setState to update our non-existent appearance.
+//     if (!mounted) return;
+  }
+
+  void _onBackgroundFetch(String taskId) async {
+// //    SharedPreferences prefs = await SharedPreferences.getInstance();
+// //    DateTime timestamp = new DateTime.now();
+//     // This is the fetch-event callback.
+//     print("[BackgroundFetch] Event received: $taskId");
+// //    setState(() {
+// //      _events.insert(0, "$taskId@${timestamp.toString()}");
+// //    });
+// //    // Persist fetch events in SharedPreferences
+// //    prefs.setString(EVENTS_KEY, jsonEncode(_events));
+//
+//     if (taskId == "flutter_background_fetch") {
+//       // Schedule a one-shot task when fetch event received (for testing).
+//       BackgroundFetch.scheduleTask(TaskConfig(
+//           taskId: "com.transistorsoft.customtask",
+//           delay: 5000,
+//           periodic: false,
+//           forceAlarmManager: true,
+//           stopOnTerminate: false,
+//           enableHeadless: true));
+//     }
+//     BackgroundFetch.finish(taskId);
+  }
+}
