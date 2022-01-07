2e7ff6ad300dccf2b9748a8b7761a71a7b063dee
honeypatel <honey.iroid@gmail.com>
2022-01-07 11:57:13 +0530

    @Honey Minor changes

--- lib/helper/flavor_config.dart
+++ lib/helper/flavor_config.dart
@@ -1,12 +1,12 @@
 class AppConfig {
   static FlavorConfig devConfig() {
-    // return FlavorConfig(webUrl: "https://knowledge-empire.com:7003/api", appName: "Knowledge Empire");//Client Development
-    return FlavorConfig(webUrl: "https://knowledge-empire.com:7001/api/", appName: "Knowledge Empire");//Client Production
+    return FlavorConfig(webUrl: "https://knowledge-empire.com:7003/api", appName: "Knowledge Empire");//Client Development
+    // return FlavorConfig(webUrl: "https://knowledge-empire.com:7001/api/", appName: "Knowledge Empire");//Client Production
   }
 
   static FlavorConfig prodConfig() {
-    // return FlavorConfig(webUrl: "https://knowledge-empire.com:7003/api", appName: "Knowledge Empire");//Client Development
-    return FlavorConfig(webUrl: "https://knowledge-empire.com:7001/api", appName: "Knowledge Empire");//Client Production
+    return FlavorConfig(webUrl: "https://knowledge-empire.com:7003/api", appName: "Knowledge Empire");//Client Development
+    // return FlavorConfig(webUrl: "https://knowledge-empire.com:7001/api", appName: "Knowledge Empire");//Client Production
   }
 }
 
